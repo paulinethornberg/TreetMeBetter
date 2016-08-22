0f93734c17313ddc650dc4651a8bf3641cbe1926
Pauline Thornberg <paulinethornberg@gmail.com>
2016-08-22 11:21:17 +0200

    Struktur och dependencies

--- src/GoodBadStuff/Startup.cs
+++ src/GoodBadStuff/Startup.cs
@@ -16,22 +16,18 @@ namespace GoodBadStuff
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // var connstring = xxxx
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+          
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
