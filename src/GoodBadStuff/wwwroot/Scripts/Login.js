ed7f08cff71fa5d9a85a6843ef6408dac6a1b305
Habala <dino_petrovic_91@hotmail.com>
2016-08-24 16:58:04 +0200

    Close button och fungerande login

--- src/GoodBadStuff/wwwroot/Scripts/Login.js
+++ src/GoodBadStuff/wwwroot/Scripts/Login.js
@@ -1,10 +1,15 @@
-﻿$("#login-submit").click(function () {
-    $.post("/User/Login", { "Username": document.getElementById("username").value, "Password": document.getElementById("password").value }, function (result) {
-        if (result === false)
-            $("#messageLabel").innerHTML = "Login failed, please try again";
-        else
-            $("#messageLabel").innerHTML = "Login mkt tng, please try again";
+﻿$(document).ready(function () {
+    $("#login-submit").click(function () {
+        $.post("/User/Login", { "Username": document.getElementById("username").value, "Password": document.getElementById("password").value }, function (result) {
+            if (result === false)
+                $("#messageLabel").innerHTML = "Login failed, please try again";
+            else
+                $("#messageLabel").innerHTML = "Login mkt tng, please try again";
             //Ändra massa greher till inloggat och sånt Gott och blandat.
-        console.log(result);
+            console.log(result);
+        });
+    });
+    $("#closeBTN").click(function () {
+        $('#myModal').modal('hide');
     });
 });
