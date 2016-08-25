67f638cc4274f90df60b666ae671152d38eb225c
Viktor <viktorhansen@me.com>
2016-08-25 11:01:31 +0200

    DINO FIXAR!

--- src/GoodBadStuff/wwwroot/Scripts/Login.js
+++ src/GoodBadStuff/wwwroot/Scripts/Login.js
@@ -7,7 +7,7 @@
                 $("#messageLabel").text("Login failed, please try again");
             else
                 $('#myModal').modal('hide');
-                //stäng rutan
+            //stäng rutan
             //Ändra massa greher till inloggat och sånt Gott och blandat.
             console.log(result);
         });
@@ -15,30 +15,15 @@
 
     //REGISTER JS
     $("#register-submit").click(function () {
-        
-        $.post("/User/Create", { "Username": $("#usernameReg").val(), "Email": $("#emailReg").val(), "Password": $("#passwordReg").val()}, function(regResult) {
+
+        $.post("/User/Create", { "Username": $("#usernameReg").val(), "Email": $("#emailReg").val(), "Password": $("#passwordReg").val() }, function (regResult) {
             if (regResult === false)
                 $("#messageLabel").text("Registration failed, please try again");
-            else
-                $("#messageLabel").text("Registration succeeded, you are logged in!");
-            $("#empty").html("<h1 style='color:green'>Registration Successful!</h1>"
+            else {
+                $("#register-form").html("<h1 style='color:green; text-align:center;'>Registration Successful!</h1>");
+                LoggedIn();
+            }
 
-                // VIKTOR FIXA DETTA!!
-
-                  <div class="row">
-                                <div class="col-xs-5">
-                                    <a href="#" class="active" id="login-form-link">Login</a>
-                                </div>
-                                <div class="col-xs-5">
-                                    <a href="#" id="register-form-link">Register</a>
-                                </div>
-                                <div class="col-xs-2">
-                                    <i class="fa fa-times fa-2x" aria-hidden="true" id="closeBTN"></i>
-                                </div>
-                            </div>
-
-
-                );
             console.log(regResult);
 
         });
@@ -51,5 +36,7 @@
     });
 });
 
- 
+function LoggedIn() {
+    $("#navBtns").html(" <li><a href='#'><span id='btn1' class='glyphicon glyphicon-user'></span> Sign Up</a></li>");
+}
 
