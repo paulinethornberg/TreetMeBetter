279c305f646594656ef957b9e7886544806353bf
Paula <Administrator@ACADEMY-61630L0>
2016-08-25 10:35:41 +0200

    fix

--- src/GoodBadStuff/wwwroot/Scripts/Login.js
+++ src/GoodBadStuff/wwwroot/Scripts/Login.js
@@ -15,13 +15,34 @@
 
     //REGISTER JS
     $("#register-submit").click(function () {
+        
         $.post("/User/Create", { "Username": $("#usernameReg").val(), "Email": $("#emailReg").val(), "Password": $("#passwordReg").val()}, function(regResult) {
             if (regResult === false)
                 $("#messageLabel").text("Registration failed, please try again");
             else
                 $("#messageLabel").text("Registration succeeded, you are logged in!");
+            $("#empty").html("<h1 style='color:green'>Registration Successful!</h1>"
+
+                // VIKTOR FIXA DETTA!!
+
+                  <div class="row">
+                                <div class="col-xs-5">
+                                    <a href="#" class="active" id="login-form-link">Login</a>
+                                </div>
+                                <div class="col-xs-5">
+                                    <a href="#" id="register-form-link">Register</a>
+                                </div>
+                                <div class="col-xs-2">
+                                    <i class="fa fa-times fa-2x" aria-hidden="true" id="closeBTN"></i>
+                                </div>
+                            </div>
+
+
+                );
             console.log(regResult);
+
         });
+
     });
 
     //CLOSE MODAL
