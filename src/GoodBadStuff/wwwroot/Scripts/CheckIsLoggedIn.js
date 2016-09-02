78dd01f73de6700436d611aee2e2156094362700
Administrator <Administrator@ACADEMY-6162X31>
2016-09-02 11:50:02 +0200

    Brutit ut checkislogged in till eget script

---
+++ src/GoodBadStuff/wwwroot/Scripts/CheckIsLoggedIn.js
@@ -0,0 +1,25 @@
+﻿// check if used is logged in 
+function checkIsLoggedIn() {
+    $.get("/user/CheckIsLoggedIn", function (loggedIn) {
+        if (loggedIn) {
+            //Create navbar buttons
+            console.log(loggedIn);
+            $("#MyTravelsButton").removeClass('none');
+            $("#MyAccountButton").removeClass('none');
+            $("#logOutButton").removeClass('none');
+            $("#saveBtnDiv").removeClass('none');
+            $("#loggBtn").addClass('none');
+        }
+        else {
+            console.log("Utloggad");
+            $("#MyTravelsButton").addClass('none');
+            $("#MyAccountButton").addClass('none');
+            $("#logOutButton").addClass('none');
+            $("#saveBtnDiv").addClass('none');
+            $("#loggBtn").removeClass('none');
+        }
+    });
+}
+$(document).ready(function () {
+    checkIsLoggedIn();
+})
--- src/GoodBadStuff/wwwroot/Scripts/Map.js
+++ src/GoodBadStuff/wwwroot/Scripts/Map.js
@@ -1,6 +1,5 @@
 ﻿$(document).ready(function () {
     initialize();
-    checkIsLoggedIn();
 });
 
 var geocoder;
@@ -27,30 +26,6 @@ function AlertSaveMessage() {
         $("#success-alert").slideUp(500);
     });
 }
-
-// check if used is logged in 
-function checkIsLoggedIn() {
-    $.get("/user/CheckIsLoggedIn", function (loggedIn) {
-        if (loggedIn) {
-            //Create navbar buttons
-            console.log(loggedIn);
-            $("#MyTravelsButton").removeClass('none');
-            $("#MyAccountButton").removeClass('none');
-            $("#logOutButton").removeClass('none');
-            $("#saveBtnDiv").removeClass('none');
-            $("#loggBtn").addClass('none');
-        }
-        else {
-            console.log("Utloggad");
-            $("#MyTravelsButton").addClass('none');
-            $("#MyAccountButton").addClass('none');
-            $("#logOutButton").addClass('none');
-            $("#saveBtnDiv").addClass('none');
-            $("#loggBtn").removeClass('none');
-        }
-    });
-}
-
 
 
 $('#goBtn').click(function () {
